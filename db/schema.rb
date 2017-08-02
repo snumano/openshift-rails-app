d15a3438e8b0f80b329567ac9f859374fa450d71
Shugo Numano <snumano@gmail.com>
2017-08-02 11:05:34 +0900

    revise for MySQL

--- db/schema.rb
+++ db/schema.rb
@@ -12,10 +12,7 @@
 
 ActiveRecord::Schema.define(version: 20170701114058) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
-  create_table "posts", force: :cascade do |t|
+  create_table "posts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "name"
     t.string "title"
     t.text "content"
